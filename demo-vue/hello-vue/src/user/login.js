e6322ccae0c509b5ded0ca4c0b71a6cdf8b8dbf8
张人大 <952402967@qq.com>
2020-05-07 20:49:50 +0800

    Upload Demo for Vue
    前端框架Vue的练习使用

---
+++ demo-vue/hello-vue/src/user/login.js
@@ -0,0 +1,9 @@
+const loginForm = { // 如果是驼峰形式的组件，在HTML中使用就需要把驼峰转化为“-”的形式
+    template:'\
+    <div>\
+    <h2>登录页</h2> \
+    用户名：<input type="text"><br/>\
+    密码：<input type="password"><br/>\
+    </div>\
+    '
+}
