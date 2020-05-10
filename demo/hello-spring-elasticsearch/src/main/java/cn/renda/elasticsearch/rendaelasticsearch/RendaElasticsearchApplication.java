a5b3e63ce309ff06f408b585ab0a923a07e02fd2
张人大 <952402967@qq.com>
2020-05-10 18:24:55 +0800

    整合Demo代码到一个文件夹

---
+++ demo/hello-spring-elasticsearch/src/main/java/cn/renda/elasticsearch/rendaelasticsearch/RendaElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package cn.renda.elasticsearch.rendaelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendaElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RendaElasticsearchApplication.class, args);
+	}
+
+}
---
+++ demo/hello-spring-elasticsearch/src/test/java/cn/renda/elasticsearch/rendaelasticsearch/RendaElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.renda.elasticsearch.rendaelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendaElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
