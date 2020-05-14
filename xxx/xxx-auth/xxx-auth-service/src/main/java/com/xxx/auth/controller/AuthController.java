2f83c7b43ba5762ac9b070bdd266e067c50e68e0
张人大 <952402967@qq.com>
2020-05-14 21:50:35 +0800

    后台实现校验用户接口
    - 定义用户的校验接口，通过cookie获取token，然后校验通过返回用户信息。 当用户在查询其个人信息，刷新token的过期时间。
    - 适当调整了 socket， http 连接，ribbon 和 hystrix 的超时时间，防止出现过于敏感的熔断。

--- xxx/xxx-auth/xxx-auth-service/src/main/java/com/xxx/auth/controller/AuthController.java
+++ xxx/xxx-auth/xxx-auth-service/src/main/java/com/xxx/auth/controller/AuthController.java
@@ -1,16 +1,16 @@
 package com.xxx.auth.controller;
 
 import com.xxx.auth.config.JwtProperties;
+import com.xxx.auth.entity.UserInfo;
 import com.xxx.auth.service.AuthService;
+import com.xxx.auth.utils.JwtUtils;
 import com.xxx.common.utils.CookieUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,8 +26,37 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private JwtProperties prop;
+
+    /**
+     * 验证用户信息
+     * @param token
+     * @return
+     */
+    @GetMapping("verify")
+    public ResponseEntity<UserInfo> verifyUser(
+            @CookieValue("XXX_TOKEN")String token,
+            HttpServletRequest request,
+            HttpServletResponse response){
+        try {
+            // 从token中解析token信息
+            UserInfo userInfo = JwtUtils.getInfoFromToken(token, this.prop.getPublicKey());
+
+            // 解析成功要重新刷新token
+            token = JwtUtils.generateToken(userInfo, this.prop.getPrivateKey(), this.prop.getExpire());
+            // 更新cookie中的token
+            CookieUtils.setCookie(request, response, this.prop.getCookieName(), token, this.prop.getCookieMaxAge());
+
+            // 解析成功返回用户信息
+            return ResponseEntity.ok(userInfo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 出现异常则，响应500
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
 
     /**
      * 登录授权
